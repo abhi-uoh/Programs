0f3761a1b45b80b35de7a5f427437bb92b21c653
Manish Kumar Choumiyan <64924907+ManishKr-Git@users.noreply.github.com>
2020-10-01 20:11:18 +0530

    Update 111.java
    Added the comma at  the end of SOP line

--- 111.java
+++ 111.java
@@ -4,15 +4,13 @@ public class Test {
       int [] numbers = {10, 20, 30, 40, 50};
 
       for(int x : numbers ) {
-         System.out.print( x );
-         System.out.print(",");
+         System.out.print( x+"," );
       }
-      System.out.print("\n");
+      System.out.println();
       String [] names = {"James", "Larry", "Tom", "Lacy"};
 
       for( String name : names ) {
-         System.out.print( name );
-         System.out.print(",");
+         System.out.print( name +"," );
       }
    }
 }
